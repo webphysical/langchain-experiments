e8b64fb50a567d8abcef7115c2c5433d64c78c04
Fabio <figueiredo.fabio@gmail.com>
2023-05-23 15:52:10 -0300

    mensagem_do_commit

--- slack/app.py
+++ slack/app.py
@@ -2,10 +2,18 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_sdk.signature import SignatureVerifier
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request
 from functions import draft_email
+from flask import Flask, request, abort
+from functions import draft_email
+import logging
+from functools import wraps
+import time
+import sys
+
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv())

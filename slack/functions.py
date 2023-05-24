e249403056c2557913243c6fc6dacf0d4e27d6d8
Fabio <figueiredo.fabio@gmail.com>
2023-05-24 13:57:41 -0300

    nova tentativa bot ptbr

--- slack/functions.py
+++ slack/functions.py
@@ -10,27 +10,27 @@ from langchain.prompts.chat import (
 load_dotenv(find_dotenv())
 
 
-def draft_email(user_input, name="Dave"):
+def draft_email(user_input, name="Fabio"):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
     template = """
     
-    You are a helpful assistant that drafts an email reply based on an a new email.
+    Você é um assistente prestativo que elabora um rascunho de resposta de e-mail com base em um novo e-mail. Você também pode responder mensagens de Whatsapp e Facebook.
     
-    Your goal is to help the user quickly create a perfect email reply.
+    Seu objetivo é ajudar o usuário a criar rapidamente uma resposta de e-mail perfeita.
     
-    Keep your reply short and to the point and mimic the style of the email so you reply in a similar manner to match the tone.
+    Mantenha sua resposta curta e direta e imite o estilo do e-mail, respondendo de maneira semelhante para combinar com o tom.
     
-    Start your reply by saying: "Hi {name}, here's a draft for your reply:". And then proceed with the reply on a new line.
+    Comece sua resposta dizendo: "Olá {name}, aqui está um rascunho para sua resposta:". E então continue com a resposta em uma nova linha.
     
-    Make sure to sign of with {signature}.
+    Certifique-se de terminar com {signature}.
     
     """
 
-    signature = f"Kind regards, \n\{name}"
+    signature = f"Atenciosamente, \n\{name}"
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
-    human_template = "Here's the email to reply to and consider any other comments from the user for reply as well: {user_input}"
+    human_template = "Aqui está o e-mail para responder e considere quaisquer outros comentários do usuário para a resposta também: {user_input}"
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
     chat_prompt = ChatPromptTemplate.from_messages(

9e42cb25c8c64d1299963e944936a464914c97e1
Fabio <figueiredo.fabio@gmail.com>
2023-05-25 10:59:02 -0300

    mensagem_do_commit

--- slack/app.py
+++ slack/app.py
@@ -1,3 +1,6 @@
+App.py
+
+
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -6,32 +9,74 @@ from slack_sdk.signature import SignatureVerifier
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request
-from functions import draft_email, analyze_sentiment_pt
-from functions import draft_email, analyze_sentiment_pt
-
+from functions import draft_email
 from flask import Flask, request, abort
-
+from functions import draft_email
 import logging
 from functools import wraps
 import time
 import sys
 
 
-# Load environment variables from .env file.
+
+
+# Load environment variables from .env file
 load_dotenv(find_dotenv())
+
 
 # Set Slack API credentials
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
 
+
 # Initialize the Slack app
 app = App(token=SLACK_BOT_TOKEN)
+
 
 # Initialize the Flask app
 # Flask is a web application framework written in Python
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
+
+
+
+
+def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API.
+    Returns:
+        str: The bot user ID.
+    """
+    try:
+        # Initialize the Slack client with your bot token
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        response = slack_client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error: {e}")
+
+
+
+
+def my_function(text):
+    """
+    Custom function to process the text and return a response.
+    In this example, the function converts the input text to uppercase.
+
+
+    Args:
+        text (str): The input text to process.
+
+
+    Returns:
+        str: The processed text.
+    """
+    response = text.upper()
+    return response
+
+
+
 
 @app.event("app_mention")
 def handle_mentions(body, say):
@@ -39,33 +84,77 @@ def handle_mentions(body, say):
     Event listener for mentions in Slack.
     When the bot is mentioned, this function processes the text and sends a response.
 
+
     Args:
         body (dict): The event data received from Slack.
         say (callable): A function for sending a response to the channel.
     """
     text = body["event"]["text"]
 
+
     mention = f"<@{SLACK_BOT_USER_ID}>"
     text = text.replace(mention, "").strip()
 
+
     say("Obrigado, Já estou trabalhando no texto!")
-    response = process_message(text)  # chamando a função process_message ao invés de draft_email
-    
-    # Envia a resposta e a análise de sentimentos
+    # response = my_function(text)
+    response = draft_email(text)
     say(response)
 
 
-def process_message(message):
-    # Realizar a análise de sentimento na mensagem original
-    sentiment = analyze_sentiment_pt(message)
 
-    # Criar o rascunho do email
-    email_draft = draft_email(message)
 
-    # Adicionar a análise de sentimento à resposta
-    response = f"{email_draft}\n\nAnálise de Sentimento da Mensagem Original: {sentiment}"
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    """
+    Route for handling Slack events.
+    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
 
-    return response
+
+    Returns:
+        Response: The result of handling the request.
+    """
+    return handler.handle(request)
+
+
+
+
+signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
+
+
+def require_slack_verification(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not verify_slack_request():
+            abort(403)
+        return f(*args, **kwargs)
+
+
+    return decorated_function
+
+
+
+
+def verify_slack_request():
+    # Get the request headers
+    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+    signature = request.headers.get("X-Slack-Signature", "")
+
+
+    # Check if the timestamp is within five minutes of the current time
+    current_timestamp = int(time.time())
+    if abs(current_timestamp - int(timestamp)) > 60 * 5:
+        return False
+
+
+    # Verify the request signature
+    return signature_verifier.is_valid(
+        body=request.get_data().decode("utf-8"),
+        timestamp=timestamp,
+        signature=signature,
+    )
+
+
 
 
 # Run the Flask app
